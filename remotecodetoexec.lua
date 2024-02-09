930f54f4b27ae2fbddf1975179329a4ba0260629
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-02-09 14:59:49 -0300

    Update remotecodetoexec.lua

--- remotecodetoexec.lua
+++ remotecodetoexec.lua
@@ -1,2 +1,2 @@
 local ReplicatedStorage = game.ReplicatedStorage
-ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('RCED', "All")
+ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer('buhh my master H1i owns me i got rced', "All")
