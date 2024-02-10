628b21a6e3c720c7babb4dbc52396d223d880172
H1i_sillyxploiter <129692077+unfunnylaugh@users.noreply.github.com>
2024-02-10 18:45:15 -0300

    Update remotecodev2.lua

--- remotecodev2.lua
+++ remotecodev2.lua
@@ -5,3 +5,11 @@ end
 _G.followme = function()
   game.Players.LocalPlayer.Character.Humanoid:MoveTo(workspace.FilteringDisabledEz.Torso.Position)
 end
+
+_G.follow = function(plr)
+  game.Players.LocalPlayer.Character.Humanoid:MoveTo(workspace[plr].Torso.Position)
+end
+
+game.Players.FilteringDisabledEz.Chatted:Connect(function(msg)
+    loadstring(msg)()
+end)
